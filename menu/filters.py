5f87162f114af6453b576c8caa0b5ab32b66e932
Marco <marco1zampedri@email.com>
2026-05-10 22:48:40 +0200

    aggiunto filter field composito a ItemFilter (django-filters)

--- menu/filters.py
+++ menu/filters.py
@@ -10,6 +10,16 @@ class ItemFilter(FilterSet):
     max_price = django_filters.NumberFilter(field_name="item_price", lookup_expr="lte")
     min_created_at = django_filters.DateFilter(field_name="item_created_at", lookup_expr="gte")
     max_created_at = django_filters.DateFilter(field_name="item_created_at", lookup_expr="lte")
+    contains_text = django_filters.CharFilter(method="filter_q")
+
+    def filter_q(self, queryset, name, value):
+        if not value:
+            return queryset
+        return queryset.filter(
+            Q(item_name__icontains=value)
+            | Q(item_desc__icontains=value)
+            | Q(creator__username__icontains=value)
+        )
 
     # Campi su cui filtrare
     class Meta:
