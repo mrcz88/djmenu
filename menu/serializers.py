537736f2ed4a0b6d0ac2dcad7d2fe94ff554abaf
Marco <marco1zampedri@email.com>
2026-05-10 22:45:50 +0200

    modificato itemserializer per tornare info su creator

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Item
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', "email"]
 
 class ItemSerializer(serializers.ModelSerializer):
+    #creator = serializers.ReadOnlyField(source='creator.username')
+    creator = UserSerializer(read_only=True)
     class Meta:
         model = Item
-        fields = ['id', 'item_name', 'item_desc', 'item_price', 'item_image']
+        fields = ['id', 'item_name', 'item_desc', 'item_price', 'item_image', 'creator']
+        read_only_fields = ['creator']
+
