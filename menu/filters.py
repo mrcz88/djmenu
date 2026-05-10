36af8e35c791e55a40e85d729f3ea8abbbc235f2
Marco <marco1zampedri@email.com>
2026-05-10 22:52:46 +0200

    fix ItemFilter: label custom field composito

--- menu/filters.py
+++ menu/filters.py
@@ -10,7 +10,7 @@ class ItemFilter(FilterSet):
     max_price = django_filters.NumberFilter(field_name="item_price", lookup_expr="lte")
     min_created_at = django_filters.DateFilter(field_name="item_created_at", lookup_expr="gte")
     max_created_at = django_filters.DateFilter(field_name="item_created_at", lookup_expr="lte")
-    contains_text = django_filters.CharFilter(method="filter_q")
+    contains_text = django_filters.CharFilter(method="filter_q", label="Text search")
 
     def filter_q(self, queryset, name, value):
         if not value:
